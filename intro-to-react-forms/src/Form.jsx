44e5d2f3b0730d231c1297fa980b6a7ecfa933ee
Steven Notovitz <sjn39@cornell.edu>
2023-03-21 11:29:51 -0400

    Update Form.jsx

--- intro-to-react-forms/src/Form.jsx
+++ intro-to-react-forms/src/Form.jsx
@@ -26,19 +26,19 @@ export default function Form() {
     const currentErrors = {};
 
     // name validation
-    if (!user.Name) currentErrors["nameError"] = "Name is missing.";
+    if (!user.Name) currentErrors["NnameError"] = "Name is missing.";
     // email validation
-    if (!user.Email || !(user.Email.split("@").length === 2)) currentErrors["emailError"] = "Email is invalid.";
+    if (!user.Email || user.Email.split("@").length !== 2) currentErrors["EmailError"] = "Email is invalid.";
     // phone number validation
-    if (!(user.PhoneNumber.length === 10)) currentErrors["phoneError"] = "PhoneNumber is invalid.";
+    if (!(user.PhoneNumber.length === 10)) currentErrors["PhoneNumberError"] = "PhoneNumber is invalid.";
     const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     for (let char of user.PhoneNumber) {
-        if (!nums.includes(char)) currentErrors["phoneError"] = "PhoneNumber is invalid.";
+        if (!nums.includes(char)) currentErrors["PhoneNumberError"] = "PhoneNumber is invalid.";
     }
     // phone type validation
-    if (user.PhoneNumber && !user.PhoneType) currentErrors["phoneTypeError"] = "Please select PhoneType.";
+    if (user.PhoneNumber && !user.PhoneType) currentErrors["PhoneTypeError"] = "Select PhoneType.";
     // bio length validation
-    if (user.Bio.length > 280) currentErrors["bioLengthError"] = "Bio has a character limit of 280.";
+    if (user.Bio.length > 280) currentErrors["BioLengthError"] = "Bio has a character limit of 280.";
 
     return currentErrors;
   };
@@ -48,7 +48,7 @@ export default function Form() {
     const currentErrors = validate();
     setErrors(currentErrors);
     if (Object.entries(currentErrors).length) console.log("errors!");
-    else console.log("valid form submission!");
+    else console.log("form submission!");
   };
 
   return (
@@ -56,17 +56,17 @@ export default function Form() {
       <form id="userform" onSubmit={handleSubmit}>
         <input type="text" placeholder="Name" value={user.Name} onChange={handleChange("Name")} />
         <br />
-        <p>{errors["nameError"]}</p>
+        <p>{errors["NameError"]}</p>
         <br />
 
         <input type="email" placeholder="Email" value={user.Email} onChange={handleChange("Email")} />
         <br />
-        <p>{errors["emailError"]}</p>
+        <p>{errors["EmailError"]}</p>
         <br />
 
         <input type="text" placeholder="PhoneNumber" value={user.PhoneNumber} onChange={handleChange("PhoneNumber")} />
         <br />
-        <p>{errors["phoneError"]}</p>
+        <p>{errors["PhoneNumberError"]}</p>
         <br />
 
         <select name="PhoneType" value={user.PhoneType} onChange={handleChange("PhoneType")}>
@@ -76,7 +76,7 @@ export default function Form() {
           <option value="Mobile">Mobile</option>
         </select>
         <br />
-        <p>{errors["phoneTypeError"]}</p>
+        <p>{errors["PhoneTypeError"]}</p>
         <br />
 
         {/* <label>Instructor
@@ -95,7 +95,7 @@ export default function Form() {
 
       <textarea name="Bio" form="userform" value={user.Bio} maxLength="280" onChange={handleChange("Bio")}>Enter text here...</textarea>
       <br />
-      <p>{errors["bioLengthError"]}</p>
+      <p>{errors["BioLengthError"]}</p>
       <br />
     </>
   );
